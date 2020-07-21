84ce13e8f74ae12fdf1b052e02bef067b4148247
Denise Mak <dpm3@uw.edu>
2020-07-21 09:13:50 -0700

    update upload example to work with sig_test_procedure updates

--- FlaskExample/data_upload_example.py
+++ FlaskExample/data_upload_example.py
@@ -2,9 +2,8 @@ from flask import Flask, url_for, redirect, request, render_template, make_respo
 from werkzeug.utils import secure_filename
 import os
 app = Flask(__name__)
-import sig_test_procedures.data_analysis as data
-
-
+import sig_test_procedures.dataAnalysis as data
+import sig_test_procedures.fileReader as filereader
 @app.route('/uploader2', methods=['GET', 'POST'])
 def upload_file2():
    if request.method == 'POST':
@@ -13,7 +12,7 @@ def upload_file2():
       f2.save(secure_filename(f2.filename))
       f.save(secure_filename(f.filename))  # todo: change this to 'uploads' directory
 
-      dicts = data.read_score_file(f.filename)
+      dicts = filereader.read_score_file(f.filename)
       rendered = render_template('da_temp.html', result1=dicts[0], result2=dicts[1])
       return rendered
 
