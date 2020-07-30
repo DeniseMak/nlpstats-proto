a95c88444285db2e30c561c2990ced98b1eafe13
Jesse Arnold Gioannini <jesse@gioannini.net>
2020-07-30 09:27:38 -0600

    removed meaninless files

--- testing/logic/report.py
+++ testing/logic/report.py
@@ -1,34 +1,18 @@
 import os
 
 def gen_report(options: dict, string: str):
-  report_string = "\n".join(["""
+  report_string = """
 \\documentclass{article}
 
 
 \\begin{document}
-""", # Preamble
 
-"""
 Data Analysis
-""", # Data Analysis
 
-"""
-
-""",
-
-"""
-
-""",
-
-"""
-
-""",
-
-"""
 \\end{document}
-""", ])
+"""
 
   os.system("echo % > user/report.tex")
   for line in ascii(report_string).split("\n"):
     os.system("echo " + line + " >> user/report.tex")
-  os.system("cd user && /Library/TeX/texbin/pdflatex report.tex report.pdf && cd .. && zip -r user/r" + string +".zip user/*")
+  os.system("cd user && /Library/TeX/texbin/pdflatex report.tex report.pdf && echo y | rm report.aux && echo y | rm report.log && cd .. && zip -r user/r" + string +".zip user/*")
