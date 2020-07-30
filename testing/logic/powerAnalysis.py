5d612dd6e266e0bcdc207f1cac28ca5db86d8e88
haz060 <37172891+haz060@users.noreply.github.com>
2020-07-30 15:12:38 -0700

    Update powerAnalysis.py

--- testing/logic/powerAnalysis.py
+++ testing/logic/powerAnalysis.py
@@ -4,7 +4,7 @@ import os
 import sys
 import random
 from scipy import stats
-import sigTesting
+import logic.sigTesting
 from statsmodels.stats.descriptivestats import sign_test
 import matplotlib
 matplotlib.use('Svg')
