f34240cdc8c9b1454bbb4d073da16cf54a1c7d18
Jesse Arnold Gioannini <jesse@gioannini.net>
2020-08-09 21:57:11 -0600

    electron prototype

---
+++ NLPStatTest-electron/main.js
@@ -0,0 +1,24 @@
+const {app, BrowserWindow} = require('electron')
+const url = require('url')
+const path = require('path')
+
+let win
+
+function createWindow() {
+   win = new BrowserWindow({width: 800, height: 600})
+   win.loadURL(url.format ({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+   }))
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  //if (process.platform !== 'darwin') {
+    app.quit()
+  //}
+})
---
+++ NLPStatTest-electron/readme.md
@@ -0,0 +1,8 @@
+## Resources
+
+So far, plumbed together from the following links:
+* Electron/Python How-to: https://www.techiediaries.com/python-electron-tutorial/ (this is what I used before to show me how python interop works)
+* Electron Tutorial: https://www.tutorialspoint.com/electron/electron_hello_world.htm
+* https://tutorialzine.com/2015/12/creating-your-first-desktop-app-with-html-js-and-electron
+* Export to PDF https://dev.to/sathish/converting-an-electron-view-content-to-pdf-caa
+* https://medium.com/@abulka/electron-python-4e8c807bfa5e
