ee4ac9c00086577fd40e4b8f80aba0cfb9909dbf
Jesse Arnold Gioannini <jesse@gioannini.net>
2020-09-11 03:28:59 -0600

    got rid of some old stuff

--- old/interface/help.py
+++
@@ -1,7 +0,0 @@
-def helper(function_name: str) -> str:
-    advice =\
-    {
-        "function 1": "Help message 1",
-        "function 2": "Help message 2",
-    }
-    return advice[function_name]
